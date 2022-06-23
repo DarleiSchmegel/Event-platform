c117eed8035c47eef9157ba178445393c427062a
DarleiSchmegel <darleischmegel94@gmail.com>
2022-06-22 23:06:03 -0300

    sidebar styles - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import { gql, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
 import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data);
-
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    <Event/>
+    // <div>
+    //   <Header />
+    //   <Sidebar />
+    //   <Video />
+    //   <Lesson />
+    // </div>
   );
 }
 
